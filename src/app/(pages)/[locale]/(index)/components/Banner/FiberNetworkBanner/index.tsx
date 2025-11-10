1cc2656c762931fbe6485e14366728d524088f38
walker27 <wzszhengzhou@hotmail.com>
2025-11-10 15:56:15 +0800

    chore: update fiber network banner

--- src/app/(pages)/[locale]/(index)/components/Banner/FiberNetworkBanner/index.tsx
+++ src/app/(pages)/[locale]/(index)/components/Banner/FiberNetworkBanner/index.tsx
@@ -11,8 +11,8 @@ export default function FiberNetworkBanner() {
   return (
     <div className={classNames(styles.banner)}>
       <div className={styles.content}>
-        <h1>{t(`banner.fiber_title`)}</h1>
-        <h3>{t(`banner.fiber_subtitle`)}</h3>
+        <h1 className="text-[18px] md:text-[40px]">{t(`banner.fiber_title`)}</h1>
+        <h3 className="text-[12px] md:text-[18px]">{t(`banner.fiber_subtitle`)}</h3>
         <div className={styles.links}>
           <Link
             href="https://www.ckbfiber.net/"
@@ -21,7 +21,7 @@ export default function FiberNetworkBanner() {
           >
             <PixelBorderBlock
               className="group"
-              contentClassName="py-0.5 px-2.5 flex flex-row items-center gap-2 text-[#fff] group-hover:text-primary"
+              contentClassName="py-0.5 px-2.5 flex flex-row items-center gap-2 text-[#fff] group-hover:text-primary text-[12px] md:text-[14px]"
               apperanceClassName="*:data-[slot=border]:bg-[#D9D9D9] *:group-hover:data-[slot=border]:bg-primary"
             >
               <span>{t(`banner.learn_more`)}</span>
