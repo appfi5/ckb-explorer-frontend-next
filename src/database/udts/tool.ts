230d0987e9410fdb11ad08b2f4d28145fce1651b
郑州 <wzszhengzhou@hotmail.com>
2026-01-04 14:34:42 +0800

    chore: restruct database and wire ckb-labels repo

--- src/database/udts/tool.ts
+++ src/database/udts/tool.ts
@@ -11,27 +11,13 @@ type UDTParams = {
   tags?: string[];
 };
 
-export class UDT {
-  public readonly name: string;
-  public readonly icon: string;
-  public readonly symbol: string;
-  public readonly decimalPlaces: number;
-  public readonly type: ScriptLike;
-  public readonly typeHash: string;
-  public readonly manager?: string;
-  public readonly tags: string[];
-  constructor(params: UDTParams) {
-    this.name = params.name;
-    this.icon = params.icon;
-    this.symbol = params.symbol;
-    this.decimalPlaces = params.decimalPlaces;
-    this.type = params.type;
-    this.manager = params.manager;
-    this.tags = params.tags || [];
-    this.typeHash = Script.from(params.type).hash();
-  }
-}
-
-export function registerUDT(args: UDTParams) {
-  return new UDT(args);
+export type UDT = {
+  name: string;
+  icon: string;
+  symbol: string;
+  decimalPlaces: number;
+  type: ScriptLike;
+  typeHash: string;
+  manager?: string;
+  tags: string[];
 }
